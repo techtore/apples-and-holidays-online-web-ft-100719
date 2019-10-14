60b9371e5c998e26acb492f52d90b1453a76b8a9
Victoria Hale <vic.hale74@gmail.com>
2019-10-14 01:05:28 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -70,6 +70,10 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  holiday_hash.each do |supply, holiday|
+  if "#{supply}" == "BBQ"
+    return "#{:holiday}"
+  end
 
 end
 
