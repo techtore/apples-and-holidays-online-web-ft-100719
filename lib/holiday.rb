96f6db0282f234fe2b8f35f977b11af6d67390d8
Victoria Hale <vic.hale74@gmail.com>
2019-10-14 01:01:13 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,7 +61,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}:"
     holiday.each do |holiday, supply|
-      puts " #{holiday.to_s.split("_").collect {|i| i.capitalize}.join(" ")}: #{supply.capitalize}.join(", ")"
+      puts " #{holiday.to_s.split("_").collect {|i| i.capitalize}.join(" ")}: #{supply.join(", ")}"
     end
   end
 
