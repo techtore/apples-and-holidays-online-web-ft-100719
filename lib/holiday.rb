051a0dcfc7a2bd93b973d23d46f0b7f3095cfe96
Victoria Hale <vic.hale74@gmail.com>
2019-10-14 01:09:36 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,7 +72,7 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   holiday_hash.map do |supply, holiday|
  holiday.map do |holiday, item|
-   return holiday if item.include?("BBQ")
+    holiday if item.include?("BBQ")
   end
 end.flatten
 end
